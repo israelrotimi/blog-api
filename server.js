34f98a28280daf7b4d48de4e6c37b9cafeace79f
Israel Rotimi <izzyrotimi@gmail.com>
2025-03-04 13:23:37 +0100

    removed body-parser and fix server

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const bodyparser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-app.use(cors);
-app.use(bodyparser);
+app.use(cors());
+app.use(express.json());
 
 const port = 3000
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>")
+  console.log("connected to root")
 });
 
 app.listen(port, () => {
